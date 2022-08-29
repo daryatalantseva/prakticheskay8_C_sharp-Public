a1a7004272785bcdf6f5176475e6d81e91df55ab
daryatalantseva <d.talantseva@gmail.com>
2022-08-29 16:08:04 +0300

    Задача 54 семинар 8

---
+++ sem8_54/Program.cs
@@ -0,0 +1,64 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+Console.WriteLine("Введите размер массива m x n");
+int m  = Convert.ToInt32(Console.ReadLine());
+int n  = Convert.ToInt32(Console.ReadLine());
+int x = InputNumbers("Введите диапазон: случайных чисел от 1 до ... ");
+
+int[,] array = new int[m, n];
+ZapolnenieArray(array);
+VivodArray(array);
+
+Console.WriteLine("Отсортированный массив: ");
+SortStrok(array);
+VivodArray(array);
+
+void SortStrok(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void ZapolnenieArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(x);
+    }
+  }
+}
+
+void VivodArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
